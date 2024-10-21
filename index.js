d79b81affc4c6d6f21ab7ae0fff572bce8a055ae
JohnPuskas <puskas.john@gmail.com>
2024-10-21 01:05:36 -0500

    Added js preventDefault() method and debugging statments for form.

---
+++ index.js
@@ -0,0 +1,13 @@
+const contactForm = document.querySelector("#contact-form");
+
+contactForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  // Below is or debugging
+  console.log("form submitted");
+  const inputs = event.target.elements;
+
+  for (let input of inputs) {
+    console.log(`input name: ${input.name}, input value: ${input.value}`);
+  }
+});

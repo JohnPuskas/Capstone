bdaa31fe63a694bc05b4cf5c5d35dd03633e380f
JohnPuskas <puskas.john@gmail.com>
2024-10-24 22:37:52 -0500

    Placed html from views within a main element. Minor html form label fixes.

--- views/home.js
+++ views/home.js
@@ -1,85 +1,92 @@
 import html from "html-literal";
 
 export default () => html`
-  <section id="user-login-section">
-    <div id="create-user" class="user-login">
-      <form name="newUser" onsubmit="">
-        <h2 class="user-heading">Create User</h2>
-        <label for="name">Username:</label>
-        <input
-          class="form-input"
-          type="text"
-          name="username"
-          placeholder="enter a new username"
-        />
-        <input class="submit-btn" type="submit" value="submit" />
-      </form>
-    </div>
-    <div id="select-user" class="user-login">
-      <form name="existingUser" onsubmit="">
-        <h2 class="user-heading">Select User</h2>
-        <label for="name">Username:</label>
-        <select class="form-input">
-          <option>To be populated by call to db.</option>
+  <main>
+    <section id="user-login-section">
+      <div id="create-user" class="user-login">
+        <form id="new-user" name="newUser" onsubmit="">
+          <h2 class="user-heading">Create User</h2>
+          <label for="new-username">Username:</label>
+          <input
+            class="form-input"
+            type="text"
+            name="new-username"
+            id="new-username"
+            placeholder="enter a new username"
+          />
           <input class="submit-btn" type="submit" value="submit" />
-        </select>
-      </form>
-    </div>
-  </section>
-  <section id="content">
-    <article id="about-app" class="app-content">
-      <h2>What Track Version Track Is:</h2>
-      <p>
-        Welcome to Track Version Track! Have you ever had trouble tracking
-        <i>what</i> changes were made in <i>which</i> versions of a song within
-        a song project? With Track Version Track, you can turn a confusing mess
-        like this:
-      </p>
-      <img
-        class="demo-image"
-        src=""
-        alt="screenshot of a confusing mess of file names"
-      />
-      <p>Into this:</p>
-      <img
-        class="demo-image"
-        src=""
-        alt="screenshot of this app's organizational feature"
-      />
-    </article>
-    <article class="app-content bottom-app-content">
-      <h2>How It Works:</h2>
-      <ol>
-        <li>
-          <p>
-            Enter titles of song projects for which you wish to track version
-            history details, and any description of the song project.
-          </p>
-        </li>
-        <li>
-          <p>
-            Click on any individual song project to enter your specific song
-            version filenames. For each filename, enter a changelog (the
-            description of changes that were made in that version)
-          </p>
-        </li>
-        <li>
-          <p>
-            View the full version history and list of changes all in one place,
-            and one page.
-          </p>
-        </li>
-        <li>
-          <p>
-            You also have the option of entering your ToDo list for the song
-            project. Add items as they come up. Cross off items as they are
-            completed, and they are saved into a Done List.
-          </p>
-        </li>
-      </ol>
-      <p>
-        That's it! You'll never again lose track of where changes were made!
-      </p>
-    </article>
-  </section>
+        </form>
+      </div>
+      <div id="select-user" class="user-login">
+        <form id="existing-user" name="existingUser" onsubmit="">
+          <h2 class="user-heading">Select User</h2>
+          <label for="existing-username">Username:</label>
+          <select
+            name="existing-username"
+            id="existing-username"
+            class="form-input"
+          >
+            <option>To be populated by call to db.</option>
+          </select>
+          <input class="submit-btn" type="submit" value="submit" />
+        </form>
+      </div>
+    </section>
+    <section id="content">
+      <article id="about-app" class="app-content">
+        <h2>What Track Version Track Is:</h2>
+        <p>
+          Welcome to Track Version Track! Have you ever had trouble tracking
+          <i>what</i> changes were made in <i>which</i> versions of a song
+          within a song project? With Track Version Track, you can turn a
+          confusing mess like this:
+        </p>
+        <img
+          class="demo-image"
+          src=""
+          alt="screenshot of a confusing mess of file names"
+        />
+        <p>Into this:</p>
+        <img
+          class="demo-image"
+          src=""
+          alt="screenshot of this app's organizational feature"
+        />
+      </article>
+      <article class="app-content bottom-app-content">
+        <h2>How It Works:</h2>
+        <ol>
+          <li>
+            <p>
+              Enter titles of song projects for which you wish to track version
+              history details, and any description of the song project.
+            </p>
+          </li>
+          <li>
+            <p>
+              Click on any individual song project to enter your specific song
+              version filenames. For each filename, enter a changelog (the
+              description of changes that were made in that version)
+            </p>
+          </li>
+          <li>
+            <p>
+              View the full version history and list of changes all in one
+              place, and one page.
+            </p>
+          </li>
+          <li>
+            <p>
+              You also have the option of entering your ToDo list for the song
+              project. Add items as they come up. Cross off items as they are
+              completed, and they are saved into a Done List.
+            </p>
+          </li>
+        </ol>
+        <p>
+          That's it! You'll never again lose track of where changes were made!
+        </p>
+      </article>
+    </section>
+  </main>
 `;

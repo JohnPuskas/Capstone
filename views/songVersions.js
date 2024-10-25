bc26e537ef759b6a4c9ba2f5f8508159e8463ebd
JohnPuskas <puskas.john@gmail.com>
2024-10-24 23:34:08 -0500

    Added a songVersions view

---
+++ views/songVersions.js
@@ -0,0 +1,11 @@
+import html from "html-literal";
+
+export default () => html`
+  <main>
+    <h1>This is the Song Versions page which will be dynamically populated</h1>
+    <h2>
+      It shows the various versions of the song selected by the user in the
+      'Songs' page
+    </h2>
+  </main>
+`;

b0c2a178672c31096f96e46da6a334a4e57b506f
JohnPuskas <puskas.john@gmail.com>
2024-10-27 20:36:31 -0500

    Added more content and styling for SongVersions page

--- views/songVersions.js
+++ views/songVersions.js
@@ -2,10 +2,68 @@ import html from "html-literal";
 
 export default () => html`
   <main>
-    <h1>This is the Song Versions page which will be dynamically populated</h1>
-    <h2>
-      It shows the various versions of the song selected by the user in the
-      'Songs' page
-    </h2>
+    <h2>This is the song Versions page which will be dynamically populated</h2>
+    <section id="versions">
+      <div class="version-container">
+        <div class="version-title-area">
+          <h3>This is a Version Title</h3>
+        </div>
+        <div class="changeLog-area">
+          <p>
+            This is a version changelog. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Magni quibusdam quod ipsum suscipit id in fugiat
+            nesciunt et aliquid minima quos, iste temporibus excepturi harum
+            sequi dignissimos laboriosam, numquam error?
+          </p>
+        </div>
+        <div class="edit-delete-buttons">
+          <button class="edit-btn">Edit</button>
+          <button class="delete-btn">Delete</button>
+        </div>
+      </div>
+      <div class="version-container">
+        <div class="version-title-area">
+          <h3>This is a Version Title</h3>
+        </div>
+        <div class="changeLog-area">
+          <p>
+            This is a version changelog. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Magni quibusdam quod ipsum suscipit id in fugiat
+            nesciunt et aliquid minima quos, iste temporibus excepturi harum
+            sequi dignissimos laboriosam, numquam error?
+          </p>
+        </div>
+        <div class="edit-delete-buttons">
+          <button class="edit-btn">Edit</button>
+          <button class="delete-btn">Delete</button>
+        </div>
+      </div>
+      <div class="version-container">
+        <div class="version-title-area">
+          <h3>This is a Version Title</h3>
+        </div>
+        <div class="changeLog-area">
+          <p>
+            This is a version changelog. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Magni quibusdam quod ipsum suscipit id in fugiat
+            nesciunt et aliquid minima quos, iste temporibus excepturi harum
+            sequi dignissimos laboriosam, numquam error?
+          </p>
+        </div>
+        <div class="edit-delete-buttons">
+          <button class="edit-btn">Edit</button>
+          <button class="delete-btn">Delete</button>
+        </div>
+      </div>
+    <section>
+
+    <div class="add">
+      <div class="add-btn">
+        <button class="add-button">
+          <i class="fa-solid fa-plus"></i>
+        </button>
+      </div>
+      <p class="add-new">Add New</p>
+    </div>
   </main>
 `;

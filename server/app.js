04ff16b0902ef215915fc38a6e60ffb75ba4214c
JohnPuskas <puskas.john@gmail.com>
2024-11-14 02:31:56 -0600

    Added CORS middleware

--- server/app.js
+++ server/app.js
@@ -2,6 +2,22 @@ import express from "express";
 
 const app = express();
 
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+app.use(cors);
 app.use(express.json());
 
 app.get("/status", (request, response) => {
